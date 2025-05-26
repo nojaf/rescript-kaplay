3d106f6ecc9e3b6be82c099328fed16d16bcc608
nojaf <florian.verdonck@outlook.com>
2025-05-26 21:00:55 +0200

    Move download to script folder

--- d.js
+++
@@ -1,3 +0,0 @@
-const name = "flareon";
-const response = await fetch(`https://img.pokemondb.net/sprites/red-blue/normal/${name}.png`);
-await Bun.write(`public/sprites/${name}-rb.png`, await response.arrayBuffer());
---
+++ scripts/download.js
@@ -0,0 +1,13 @@
+const args = Bun.argv;
+const lastArg= args[args.length - 1];
+
+if (lastArg.endsWith(".js")) {
+    console.log("Add a pokemon name as last argument to download the sprite");
+    process.exit(1);
+} else {
+    const name = lastArg;
+    const response = await fetch(`https://img.pokemondb.net/sprites/red-blue/normal/${name}.png`);
+    await Bun.write(`public/sprites/${name}-rb.png`, await response.arrayBuffer());
+    console.log(`Sprite downloaded for ${name}`);
+}
+
